b97d177f5e7d4a536bdcb6226fab0823f068daf5
Ethann Schneider <ethann@shkermit.ch>
2024-05-06 15:29:37 +0200

    #2 Added home module page on the website

--- src/main/angular/src/app/app.component.ts
+++ src/main/angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  public static title: string;
 
+  get title() {
+    return AppComponent.title;
+  }
 }
--- src/main/angular/src/app/home/home.component.ts
+++ src/main/angular/src/app/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
+import { KanbanService } from '../@core/service/kanban.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  boards: any = [];
 
+  constructor(
+    private kanbanService: KanbanService,
+  ) { }
+  
+  ngOnInit(): void {
+    AppComponent.title = 'Kanban Boards';
+
+    this.kanbanService.getBoards().subscribe((boards: any) => {
+      this.boards = boards;
+    });
+  }
+
+  generateColor(color: string): string {
+    return "background-color: #" + color + ";"
+  }
 }
