f5e7884a7b373349f997c38ffcecb44c3b17c777
Ethann Schneider <ethann@shkermit.ch>
2024-05-14 14:28:39 +0200

    #5 Added backlog page

--- src/main/angular/src/app/kanban/backlog/backlog.component.ts
+++ src/main/angular/src/app/kanban/backlog/backlog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { KanbanService } from '../../@core/service/kanban.service';
 import { AppComponent } from '../../app.component';
+import { TaskService } from '../../@core/service/task.service';
 
 @Component({
   selector: 'app-backlog',
@@ -9,13 +10,15 @@ import { AppComponent } from '../../app.component';
   styleUrl: './backlog.component.scss'
 })
 export class BacklogComponent implements OnInit{
+  backlogTasks: any = []
   name: string = ''
   backlogRedirect: string = ''
   boardRedirect: string = ''
   
   constructor(
     private route: ActivatedRoute,
-    private kanbanService: KanbanService
+    private kanbanService: KanbanService,
+    private taskService: TaskService
   ) { }
 
   ngOnInit(): void {
@@ -32,10 +35,32 @@ export class BacklogComponent implements OnInit{
     this.kanbanService.getBoard(this.name).subscribe({
       next: kanban => {
         AppComponent.title = this.name
+        this.updateTasks();
       },
       error: error => {
         window.location.href = '/'
       }
     });
+  }
+
+  updateTasks() {
+    this.backlogTasks = []
+    this.taskService.getTasks(this.name).subscribe({
+      next: tasks => {
+        const body = tasks as [{ [key: string]: string }];
+        for (let task of body) {
+          if (task["status"] == 'BACKLOG') {
+            this.backlogTasks.push(task)
+          }
+        }
+      },
+      error: error => {
+        window.location.href = '/'
+      }
+    });
+  }
+
+  gotoTasksDetails(taskName: string) {
+    window.location.href = `/kanban/${this.name}/task/${taskName}`
   }
 }
